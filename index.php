58ea05923e9f9cd94a158fb47304cc0aa619fb84
L.Sliva <9016232@student.zadkine.nl>
2022-04-22 11:37:45 +0200

    Adding all files

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Polo 3 Blackjack</title>
+    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="game.css">
+    <link rel="stylesheet" href="main.js">
+</head>
+
+<body>
+<div class="alleshomepage">
+    <div class="sponsors-sitelogo">
+        <div class="homepagediv">
+            <img class="logo" src="assets/logo.png"/>
+        </div>
+        <div class="sponsors">
+            <img class=sponsorslogo" src="assets/Sponsors.png" height="400" width="200"/>
+        </div>
+    </div>
+    <div class="newgamebutton">
+        <a href="play.php">
+            <button class="bn632-hover bn28">New Game</button>
+        </a>
+    </div>
+    <div class="paymentmethods">
+        <img class=payments" src="assets/payment-methods-hibs.png" width="350px" height="75px"/>
+    </div>
+</div>
+
+
+</body>
+</html>
